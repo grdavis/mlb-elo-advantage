a15b7d853909761e0e9378244db22430b4bb6fc6
grdavis <s14gdavis@gmail.com>
2024-07-29 12:29:54 -0400

    Update predictions.py adding eval_recent_performance function

--- predictions.py
+++ predictions.py
@@ -2,12 +2,57 @@ import elo
 import utils
 from datetime import datetime
 import pandas as pd
-from analysis import eval_recent_performance
 
 OUTPUT_PATH = f"OUTPUTS/{utils.date_to_string(datetime.today())[:10]} Game Predictions.csv"
 ADV_THRESHOLD = .04
 ADV_PCT_THRESHOLD = .11 #changed from .08 on 7/28/24 after more tuning adjustments to home advantage 
 ADV_TO_USE = 'ADV_PCT'
+
+def assemble_results_and_predictions():
+    #grab the ratings and predictions for every historical game then enrich the dataset
+    latest_df = pd.read_csv(utils.get_latest_data_filepath())
+    latest_sim, combo = elo.sim(latest_df, elo.K_FACTOR, elo.HOME_ADVANTAGE)
+    combo = combo.loc[combo['Date'] < utils.today_date_string()]
+
+    #convert the ML odds to an implied probability, they will sum to more than 100% because of the book's vig
+    combo['H_ODDS'] = combo.apply(lambda x: utils.odds_calc(x['Home_ML']), axis = 1)
+    combo['A_ODDS'] = combo.apply(lambda x: utils.odds_calc(x['Away_ML']), axis = 1)
+
+    #calculate the "advantage" for betting on a team as the ELO probability minus the implied probability
+    combo['H_ADV'] = combo['HOME_PRE_PROB'] - combo['H_ODDS']
+    combo['A_ADV'] = combo['AWAY_PRE_PROB'] - combo['A_ODDS']
+
+    #calculate an alternative "advantage" as the ELO probability's percentage difference from the implied probabiltiy
+    combo['H_ADV_PCT'] = combo['HOME_PRE_PROB'] / combo['H_ODDS'] - 1
+    combo['A_ADV_PCT'] = combo['AWAY_PRE_PROB'] / combo['A_ODDS'] - 1
+
+    #convert the ML numbers to a profit multiple and calculate how much we would have profited if we bet on one of the sides
+    combo['H_WAGER'] = combo.apply(lambda x: utils.wager_calc(x['Home_ML']), axis = 1)
+    combo['A_WAGER'] = combo.apply(lambda x: utils.wager_calc(x['Away_ML']), axis = 1)
+    combo['H_PROFIT'] = combo.apply(lambda x: utils.profit_calc(x['Home_ML']) if int(x['Home_Score']) > int(x['Away_Score']) else -x['H_WAGER'], axis = 1)
+    combo['A_PROFIT'] = combo.apply(lambda x: utils.profit_calc(x['Away_ML']) if int(x['Away_Score']) > int(x['Home_Score']) else -x['A_WAGER'], axis = 1)
+
+    return combo
+
+def convert_to_betting_rows(combo, adv_to_use):
+    #convert dataframe from one row per game to one row per possible betting side (2 rows per game)
+    mdf = pd.melt(combo, id_vars = ['H_WAGER', 'A_WAGER', 'H_PROFIT', 'A_PROFIT'], value_vars = [f'H_{adv_to_use}', f'A_{adv_to_use}'])
+    mdf['WAGER'] = mdf.apply(lambda x: x['H_WAGER'] if x['variable'] == f'H_{adv_to_use}' else x['A_WAGER'], axis = 1)
+    mdf['PROFIT'] = mdf.apply(lambda x: x['H_PROFIT'] if x['variable'] == f'H_{adv_to_use}' else x['A_PROFIT'], axis = 1)
+    mdf.rename(columns = {'value': 'ADVANTAGE'}, inplace = True)
+    return mdf
+
+def eval_recent_performance(recent_days, adv_to_use, threshold):
+    #evaluate performance of betting thresholds over recent days
+    combo = assemble_results_and_predictions()
+    combo = combo.loc[combo['Date'] >= utils.shift_dstring(today_date_string(), -recent_days)]
+    
+    mdf = convert_to_betting_rows(combo, adv_to_use)
+    total_games = mdf.shape[0] / 2 #divide by two since mdf has one row for each side of the game
+
+    #return the ROI and bet rate with current threshold used for predictions
+    aggs = mdf.loc[mdf['ADVANTAGE'] >= threshold][['WAGER', 'PROFIT']].agg(['size', 'sum'])
+    return (round(aggs.loc['sum', 'PROFIT'] / aggs.loc['sum', 'WAGER'] * 100, 2), round(aggs.loc['size', 'WAGER'] / total_games * 100))
 
 def odds_needed(winp, adv_type):
 	'''
