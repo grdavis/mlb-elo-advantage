169c5b1d8e6405eaaa1fb4a7c6c44737500bbb71
grdavis <s14gdavis@gmail.com>
2024-10-07 07:29:08 -0700

    Update season_predictions.py

--- season_predictions.py
+++ season_predictions.py
@@ -105,7 +105,7 @@ def setup_playoffs(this_sim):
 		divw = {'AL': ['NYY', 'CLE', 'HOU'], 'NL': ['LAD', 'PHI', 'MIL']}
 		wc_round = [('LAD', 2), ('LAD', 0), ('SDP', 2), ('ATL', 0), ('PHI', 2), ('PHI', 0), ('MIL', 1), ('NYM', 2), 
 					('NYY', 2), ('NYY', 0), ('BAL', 0), ('KCR', 2), ('CLE', 2), ('CLE', 0), ('HOU', 0), ('DET', 2)]
-		div_round = [('LAD', 1), ('SDP', 0), ('PHI', 0), ('NYM', 1), 
+		div_round = [('LAD', 1), ('SDP', 1), ('PHI', 1), ('NYM', 1), 
 					('NYY', 1), ('KCR', 0), ('CLE', 1), ('DET', 0)]
 		#will define each of these as the 2024 playoffs go on
 		# league_round = None
